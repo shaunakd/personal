cbcc01be1d5cdc5fce38e019aed5e3d787565a9a
shaunakd <31369749+shaunakd@users.noreply.github.com>
2024-09-04 11:35:55 +0100

    Create get_longest_common_prefix function

---
+++ trading_interview/assignments/7_longest_common_prefix.py
@@ -0,0 +1,13 @@
+import re
+
+def get_longest_common_prefix(words: list[str]) -> str:
+  """Returns a string representing the longest common prefix among all the strings in the list"""
+  longest_common_prefix = ""
+  for char_tuple in zip(words):
+    if len(set(char_tuple)) == 1:
+      # adds to longest_common_prefix if char is in all of the words
+      longest_common_prefix += char_tuple[0]
+    else:
+      # ends the loop otherwise
+      break
+  return longest_common_prefix
