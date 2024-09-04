3511230eb70eaeadb64e9615c98b501c0ecc96cf
shaunakd <31369749+shaunakd@users.noreply.github.com>
2024-09-04 10:27:19 +0100

    Created missing_number function

--- trading_interview/assignments/4_missing_number.py
+++ trading_interview/assignments/4_missing_number.py
@@ -1,1 +1,4 @@
-
+def find_missing_number(numbers: list[int]) -> int:
+  """Finds the single missing number in a given array supposed to contain all integers from 1 to n."""
+  missing_number = sum(range(max(numbers))) - sum(numbers) # all terms will cancel out except the missing term
+  return missing_number
