23f053a6d39fbd0469b1ccc0cc32e2ee6256fdca
shaunakd <shaunakd@gmail.com>
2024-09-05 08:14:49 +0100

    did some more reorganisation of files

---
+++ projects/python_projects/trading_interview/assignments/words_with_all_vowels.py
@@ -0,0 +1,8 @@
+"""Project 2"""
+
+def get_words_with_all_vowels(words: list[str]) -> list[str]:
+  words_with_all_vowels = [
+    word for word in words
+    if {'a', 'e', 'i', 'o', 'u'} <= set(word)
+  ]
+  return words_with_all_vowels
