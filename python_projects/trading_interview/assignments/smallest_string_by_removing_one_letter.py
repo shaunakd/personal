4cfc65ea3acca75fdb14bc14a211de59a3f0280e
shaunakd <shaunakd@gmail.com>
2024-09-05 08:14:35 +0100

    did some more reorganisation of files

--- python_projects/trading_interview/assignments/smallest_string_by_removing_one_letter.py
+++
@@ -1,8 +0,0 @@
-"""Project 5"""
-
-def get_smallest_string_by_removing_one_letter(s: str) -> str:
-  """Returns the alphabetically smallest string that can be obtained by removing exactly one letter from s."""
-  max_letter = max(s)
-  # removes the maximum letter
-  smallest_string = s.replace(max_letter, "", 1)
-  return smallest_string
