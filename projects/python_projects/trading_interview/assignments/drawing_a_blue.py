1599c2c6355cee0ac04f850e73fc1f05fc63be25
shaunakd <shaunakd@gmail.com>
2024-09-07 09:25:36 +0100

    added .venv

--- projects/python_projects/trading_interview/assignments/drawing_a_blue.py
+++ projects/python_projects/trading_interview/assignments/drawing_a_blue.py
@@ -1,5 +1,6 @@
 """Project 9"""
 
+import random
 from typing import Optional
 
 def get_prob_blue_given_n(num_of_red_cards: int, num_of_blue_cards: int, n: Optional[int]) -> float:
@@ -10,3 +11,14 @@ def get_prob_blue_given_n(num_of_red_cards: int, num_of_blue_cards: int, n: Opti
   num_of_blue_cards = (num_of_blue_cards + 1)/2
   prob_blue_given_n = 0
   return prob_blue_given_n
+
+
+def simulate(n_cards: int) -> None:
+  total_3_count, blue_3_count = 0, 0
+  
+  # generating red and blue cards
+  red_cards, blue_cards = list(range(1, n_cards + 1)), list(range(1, n_cards + 1))
+  
+  # removing (n_cards + 1)/2 cards
+  blue_cards_to_remove = random
+  
