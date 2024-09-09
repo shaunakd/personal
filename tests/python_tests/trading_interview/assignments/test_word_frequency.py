2370d0f9d9ab171f2b2bf7883443d2b5bcfe549b
shaunakd <shaunakd@gmail.com>
2024-09-09 08:34:49 +0100

    commit

--- tests/python_tests/trading_interview/assignments/test_word_frequency.py
+++ tests/python_tests/trading_interview/assignments/test_word_frequency.py
@@ -32,7 +32,7 @@ def test_filter_text(test_dir, expected_output):
     assert actual_output == expected_output
     if actual_output:
         with open(ROOT_DIR / "filtered_text.txt", "w+") as file:
-            if actual_output != file.read():
+            if f"{actual_output}\n" != file.read():
                 file.write(actual_output)
 
 
